ff7a3cc28dfb9a2e1ec1716e5a4d1c6572e7ad62
Yuri Rybak <w.yuri.rybak@gmail.com>
2026-03-06 11:45:46 -0500

    Allow X/Twitter to iframe embed pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/embed/:path*",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
